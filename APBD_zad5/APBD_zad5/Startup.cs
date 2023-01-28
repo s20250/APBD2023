71b337d67b413e290470df0cd1950f20aa0623bc
s20250 <101040248+s20250@users.noreply.github.com>
2023-01-28 20:02:19 +0100

    Update Startup.cs

--- APBD_zad5/APBD_zad5/Startup.cs
+++ APBD_zad5/APBD_zad5/Startup.cs
@@ -1,6 +1,49 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using APBD_zad5.Services;
+
 namespace APBD_zad5;
 
 public class Startup
-{
-    
-}
+
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IDBService, WarehouseDBService>();
+            services.AddRouting(options => options.LowercaseUrls = true);
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("1", new OpenApiInfo { Title = "Warehouses", Version = "v1.0" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "warehouse v1.0"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
